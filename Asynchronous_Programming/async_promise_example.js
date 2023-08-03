993c4fdd880daeae46818c777a6c0df7a4ee61c4
umair.tariq <umair.tariq@techno-soft.com>
2023-08-03 11:43:20 +0500

    promise and promise chaining example to resolve callback hell problem

--- Asynchronous_Programming/async_promise_example.js
+++ Asynchronous_Programming/async_promise_example.js
@@ -1,9 +1,8 @@
-
 const getData = (resource) => {
     return new Promise((resolve, reject) => {
         const request = new XMLHttpRequest();
         request.addEventListener("readystatechange", () => {
-            // console.log(request, request.readyState);
+            console.log(request, request.readyState);
             if (request.readyState === 4 && request.status === 200) {
                 //   console.log(request.responseText);
                 // callback(undefined, request.responseText);
@@ -20,10 +19,42 @@ const getData = (resource) => {
 };
 
 
-getData('https://jsonplaceholder.typicode.com/todos/1sdfgsdfg').then(
+getData('https://jsonplaceholder.typicode.com/todos/1').then(
     (data)=>{
         console.log("Promise resolved:\n"+data)
     }
 ).catch((err) =>{
     console.log('Promise rejected:\n'+err)
 })
+
+
+console.log(1)
+console.log(2)
+
+// promise chaining example
+
+// Let`s suppose our we fetch data and then this data is processed by another data and then the processed data is used by another function
+getData('https://jsonplaceholder.typicode.com/todos/1').then(
+    (data) => {
+        console.log('Promise 1 resolved:', data)
+        return getData('https://jsonplaceholder.typicode.com/todos/2')
+    }
+).then(
+    (data) => {
+        console.log('Promise 2 resolved:', data)
+        return getData('https://jsonplaceholder.typicode.com/todos/3')
+    }
+).then(
+    (data)=> {
+        console.log('Promise 3 resolved:', data)
+    }
+).catch(err =>{
+    console.log('Promise Rejected',err)
+})
+
+// This single catch is can catch the error from any then
+
+
+
+console.log(3)
+console.log(4)
