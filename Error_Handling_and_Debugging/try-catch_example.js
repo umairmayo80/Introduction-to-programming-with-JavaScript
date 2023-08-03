10f982bbc5c79f42d9503a3a72d3114fb6a0aef5
umair.tariq <umair.tariq@techno-soft.com>
2023-08-03 16:19:05 +0500

    Try and Catch example

--- Error_Handling_and_Debugging/try-catch_example.js
+++ Error_Handling_and_Debugging/try-catch_example.js
@@ -9,46 +9,47 @@
 //   }
 
 function divide(a, b) {
-    try {
-      if (b === 0) {
-        throw new Error("Division by zero is not allowed.");
-      }
-      return a / b;
-    } catch (error) {
-      console.error("An error occurred:", error.message);
-      // You can choose to handle the error here or re-throw it to the calling context.
-      // throw error; // Uncomment this line to re-throw the error.
+  try {
+    if (b === 0) {
+      throw new Error("Division by zero is not allowed.");
     }
+    return a / b;
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+    // You can choose to handle the error here or re-throw it to the calling context.
+    // throw error; // Uncomment this line to re-throw the error.
   }
-  
-  console.log(divide(10, 0)); // Output: An error occurred: Division by zero is not allowed.
-  
+}
 
-
+console.log(divide(10, 0)); // Output: An error occurred: Division by zero is not allowed.
 
 //   You can use the finally block to execute code that needs to run regardless of whether an error occurred or not.
 
-
 function fetchData() {
-    try {
-      // Fetch data from the server
-      return response;
-    } catch (error) {
-      console.error("An error occurred:", error.message);
-    } finally {
-      console.log("Cleanup and resource release.");
-    }
+  try {
+    // Fetch data from the server
+    return response;
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  } finally {
+    console.log("Cleanup and resource release.");
   }
-  
-  fetchData();
-  
+}
 
-
+fetchData();
 
 //   Catch binding
 try {
-    throw new TypeError("oops");
-  } catch ({ name, message }) {
-    console.log(name); // "TypeError"
-    console.log(message); // "oops"
-  }
+  throw new TypeError("oops");
+} catch ({ name, message }) {
+  console.log(name); // "TypeError"
+  console.log(message); // "oops"
+}
+
+// same as
+try{
+    throw new ReferenceError("Message content")
+} catch (err){
+    console.log(err.name)
+    console.log(err.message)
+}
